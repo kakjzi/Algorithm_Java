d00a79c79248072a0368cb14c0312789cb475246
kakjzi <anmern3@naver.com>
2021-12-26 23:24:14 +0900

    ADD: Inf_1

---
+++ src/JavaInf_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JavaInf_1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String input1 = in.next();
+        char input2 = in.next().charAt(0);
+        JavaInf_1 t = new JavaInf_1();
+        System.out.println(t.solution(input1, input2));
+    }
+
+    public int solution(String str, char ch) {
+        int answer = 0;
+        str = str.toLowerCase();
+        ch = Character.toLowerCase(ch);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                answer ++;
+            }
+        }
+        return answer;
+    }
+}
