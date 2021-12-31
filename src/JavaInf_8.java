bab24a179d5e0d0278a395b99ff7ee3570dad1ef
kakjzi <anmern3@naver.com>
2021-12-31 23:39:44 +0900

    ADD: 문제풀이중...

---
+++ src/JavaInf_8.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class JavaInf_8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine().trim().toLowerCase();
+        if(solution(input)){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static Boolean solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, str.length())
+                .filter(i -> Character.isAlphabetic(str.charAt(i)))
+                .forEach(i -> sb.append(str.charAt(i)));
+
+        String reverseStr = sb.reverse().toString();
+        return reverseStr.equals(sb.toString());
+
+    }
+
+}
