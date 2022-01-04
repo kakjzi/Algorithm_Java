194a21ecf28527c2eab1bc0645c2ba1adee5e1a7
kakjzi <anmern3@naver.com>
2022-01-05 01:46:16 +0900

    ADD: 보이는 학생

---
+++ InfJava/JavaInf_14.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JavaInf_14 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        //String[] splitStr = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(arr));
+    }
+
+    /**
+     * 접근방식
+     * 배열을 정렬 후 비교 대상보다 크면 OK
+     */
+    public static int solution(int[] arr) {
+        int count = 1;
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
