2e77f4ff584abce7d6dbe8a0fc990abb70dc6d36
kakjzi <anmern3@naver.com>
2022-01-09 01:57:02 +0900

    ADD: 타겟넘버 DFS

--- Programmers/Level2/_20220108.java
+++ Programmers/Level2/_20220108.java
@@ -1,6 +1,29 @@
 package Programmers.Level2;
 
-
 public class _20220108 {
+
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+
+    public int dfs(int[] numbers, int target, int result, int idx) {
+        //재귀 호출 종료조건
+        if (numbers.length == idx) {
+            if (target == result) {
+                return 1;
+            }
+            return 0;
+        }
+
+        int curPlus = result + numbers[idx];
+        int curMinus = result - numbers[idx];
+        int answer = 0;
+
+        answer += dfs(numbers, target, curPlus, idx + 1);
+        answer += dfs(numbers, target, curMinus, idx + 1);
+
+        return answer;
+    }
 }
+
 
