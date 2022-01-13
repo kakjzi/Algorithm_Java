7dcdf6008abe0dd8d2c49abd71922f41af53a673
kakjzi <anmern3@naver.com>
2022-01-14 00:36:10 +0900

    ADD: 행렬 테두리 회전하기

---
+++ Programmers/Level2/_20220114.java
@@ -0,0 +1,56 @@
+package Programmers.Level2;
+
+public class _20220114 {
+
+    private int[][] matrix;
+
+    public int[] solution(int rows, int columns, int[][] queries) {
+        matrix = new int[rows][columns];
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = i * columns + j + 1;
+            }
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            answer[i] = rotate(queries[i]);
+        }
+        return answer;
+    }
+
+    public int rotate(int[] query) {
+        int x1 = query[0] - 1;
+        int y1 = query[1] - 1;
+        int x2 = query[2] - 1;
+        int y2 = query[3] - 1;
+
+        int temp = matrix[x1][y1];
+        int min = temp;
+
+        //좌
+        for (int i = x1; i < x2; i++) {
+            matrix[i][y1] = matrix[i + 1][y1];
+            min = Math.min(min, matrix[i][y1]);
+        }
+        //상
+        for (int i = y2; i > y1; i--) {
+            matrix[x1][i] = matrix[x1][i - 1];
+            min = Math.min(min, matrix[x1][i]);
+        }
+        // 우
+        for (int i = x2; i > x1; i--) {
+            matrix[i][y2] = matrix[i - 1][y2];
+            min = Math.min(min, matrix[i][y2]);
+        }
+        //하
+        for (int i = y1; i < y2; i++) {
+            matrix[x2][i] = matrix[x2][i + 1];
+            min = Math.min(min, matrix[x2][i]);
+        }
+        matrix[x1][y1 + 1] = temp;
+
+        return min;
+    }
+}
