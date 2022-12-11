8ef3ee65e961bdf1faedcf09be236d9b73b159d9
kakjzi <82758364+kakjzi@users.noreply.github.com>
2022-12-11 23:02:42 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.49 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int slice, int n) {
+      int answer = 0;
+      int p = (int)Math.ceil((double)n/slice);
+        
+      answer=p;
+      return answer;
+    }
+}
