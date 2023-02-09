8adf3e8d90abceff0946f21adc2206e5ad69c33c
kakjzi <82758364+kakjzi@users.noreply.github.com>
2023-02-09 23:16:54 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 268 ms, Memory: 36568 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,29 +1,32 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
+	static char c;
+	static String s;
+	static int q, l, r;
+	static int[][] alphaList = new int[26][200000];
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		int n = s.length();
-		int[][] alphabets = new int[n][26];
-
-		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n; j++) {
-				alphabets[j][s.charAt(i) - 'a']++;
-			}
+		s = br.readLine();
+		q = Integer.parseInt(br.readLine());
+		for (int i = 0; i < s.length(); i++) {
+			for (int alpha = 0; alpha < 26; alpha++)
+				alphaList[alpha][i] = (i == 0) ? 0 : alphaList[alpha][i - 1];
+			alphaList[s.charAt(i) - 'a'][i]++;
 		}
-		int q = Integer.parseInt(br.readLine());
-
-		for (int i = 0; i < q; i++) {
+		while (q-- > 0) {
 			String[] line = br.readLine().split(" ");
-			char alp = line[0].charAt(0);
-			int l = Integer.parseInt(line[1]);
-			int r = Integer.parseInt(line[2]);
-			int lValue = (l > 0) ? alphabets[l - 1][alp - 'a'] : 0;
-			int rValue = alphabets[r][alp - 'a'];
-			System.out.println(rValue - lValue);
+			c = line[0].charAt(0);
+			l = Integer.parseInt(line[1]);
+			r = Integer.parseInt(line[2]);
+			System.out.println((l == 0) ? alphaList[c - 'a'][r] : alphaList[c - 'a'][r] - alphaList[c - 'a'][l - 1]);
 		}
 	}
+
 }
+
+
+
