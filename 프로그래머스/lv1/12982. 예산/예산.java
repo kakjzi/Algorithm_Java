e4f2308c42bca647a0d404ad83d31295f899c9f4
kakjzi <82758364+kakjzi@users.noreply.github.com>
2023-03-20 23:50:54 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+ 
+public class Solution {
+    public int solution(int[] d, int budget) {
+        int sum = 0;
+        int count =0 ;
+        Arrays.sort(d);
+        
+        for(int i = 0; i < d.length; i++) {
+            if(sum + d[i] > budget) {
+                break;
+            }
+            
+            sum += d[i];
+            count++;
+        }
+        return count;
+    }
+}
